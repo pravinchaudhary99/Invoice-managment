87b8229495ed0d1ff582f49e9f74828f39c88322
Nainesh <nkrabadiya@gmail.com>
2023-07-07 19:28:15 +0530

    add migrations

---
+++ database/migrations/2023_07_07_135125_create_invoices_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->id();
+            $table->string('invoice_number')->nullable();
+            $table->date('start_date');
+            $table->date('due_date');
+            $table->string('address');
+            $table->string('state');
+            $table->string('zip_code');
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('invoices');
+    }
+};
